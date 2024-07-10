9763f09277bc8638e5569938557f5a104fb3f4d1
Victor Dircio <73125400+vdircio@users.noreply.github.com>
2024-07-10 22:18:26 +0000

    initial commit

---
+++ debugger.rb
@@ -0,0 +1,7 @@
+require "pry-byebug"
+
+f = "Your lucky number is "
+l = rand(100).to_s
+
+# byebug
+pp f + l
---
+++ howdy.rb
@@ -0,0 +1,6 @@
+require "active_support/all"
+pp "hey"
+require "./goodbye.rb"
+
+pp 10.ordinalize
+pp "jpg".pluralize
